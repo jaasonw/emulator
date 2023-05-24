b92b475d8cadb27b35e22014300e19deacb2d06c
jaasonw <jayywong92@gmail.com>
2023-05-24 02:16:00 -0700

    the codegen evolves

--- util/opcode_codegen.py
+++ util/opcode_codegen.py
@@ -11,6 +11,12 @@ rust_code = "match opcode {\n"
 for opcode, data in opcodes["cbprefixed"].items():
     rust_code += f"    {opcode} => {{\n"
     rust_code += f"        // {data['mnemonic']} {' '.join([operand['name'] for operand in data['operands']])}\n"
+    if data["mnemonic"] == "BIT":
+        rust_code += f"            if get_bit_at_position_{16 if 'HL' in [operand['name'] for operand in data['operands']] else 8}bit(3, gb.cpu.get_{[operand['name'] for operand in data['operands']][1].lower()}()) == 1 {{\n"
+        rust_code += f"                gb.cpu.set_z_flag(true);\n"
+        rust_code += "            } else {\n"
+        rust_code += f"                gb.cpu.set_z_flag(false);\n"
+        rust_code += "            }\n"
     if len(data["cycles"]) > 1:
         rust_code += "        // if branch\n"
         rust_code += f"        cycles += {data['cycles'][0]};\n"
